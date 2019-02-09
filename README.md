0f635a2ea328a1439c24758a92e0bc308f0e9037
Elijah Helmandollar <elijahhelmandollar101@gmail.com>
2019-02-08 21:07:37 -0500

    Test commit on Windows

---
+++ README.md
@@ -0,0 +1,2 @@
+"# Mardi" 
+"# mardi" 
---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, default='Cowboy Bebop: Ultimate Anime Analysis')
+    desc = models.TextField(max_length=250, default='A simple description will suffice')
+    body = models.TextField(default='This is where the content of the post will go')
+    link = models.TextField(default='For example: https://www.roblox.com/NewLogin')
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+
+    def __str__(self):
+        return self.title
